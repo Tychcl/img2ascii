88d96d275f7f65bdf2d8930e758c98036d5bf0e6
Tychcl <tocabloha@gmail.com>
2025-07-27 17:38:18 +0500

    ready for post

--- test.py
+++ test.py
@@ -9,44 +9,31 @@ RatioX = 14/7
 RatioY = 7/14
 
 CharSize = {"x" : 8, "y" : 8} #хранит размер символа по х и у
-FontRatioX = {"cour.ttf":1 , "consolas.ttf":0.80108695652}
 
-def resize_image(original_image: Image, size: tuple) -> Image:
-    if size is None:
-        return original_image.resize((round(original_image.width / CharSize["x"] * RatioX), round(original_image.height / CharSize["y"])), Image.LANCZOS)
-    else:
-        return original_image.resize(size, Image.LANCZOS)
+def resize_image(image: Image, size: tuple) -> Image:
+    return image.resize(size, Image.LANCZOS), image.resize((round(size[0] * RatioX), size[1]), Image.LANCZOS)
     
 def mono_image(resized_image: Image) -> Image:
     return resized_image.convert("L")
 
-def ascii_image(gray_scale_image: Image) -> str:
-    ascii_str = ""
+def ascii_image(gray_scale_image: Image) -> list[str]:
+    ascii_list = []
     dv = 255 / (len(CharSet)-1)
     for y in range(gray_scale_image.height):
+        ascii_str = ""
         for x in range(gray_scale_image.width):
             ascii_str += CharSet[round(gray_scale_image.getpixel((x, y)) / dv)]
-        ascii_str += "\n"
-    return ascii_str
+        ascii_list.append(ascii_str)
+    return ascii_list #[line.strip() for line in ascii_str.split('\n') if line.strip()]
 
-def ascii2image_monochrome(ascii_str: str, background: tuple, foreground: tuple) -> str | None:
-    lines = [line.rstrip() for line in ascii_str.split('\n') if line.strip()]
-    if not lines:
-        return None
-    ascii_str = "\n".join(lines)
-    width = max(len(line) for line in lines)
-    height = len(lines)
-    RX = width / height
-    font = "cour.ttf"
-    img = Image.new("RGB", (round(width * 5 * FontRatioX[font]), round(height * CharSize["y"] * RX)), background)
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype(f"resources/fonts/{font}", CharSize["x"])
-    draw.multiline_text((0,0), ascii_str, font=font, fill=foreground)
-    save = f"result/{FileInfo["Name"]}/{FileInfo["Name"]}.{FileInfo["Format"]}"
-    img.save(save)
-    return save
+def ascii2image_monochrome(ascii_list: list[str], color_invert: bool) -> str | None:
+    width = max(len(line) for line in ascii_list)
+    height = len(ascii_list)
+    print( width, height)
 
-def convert_image(path2image: str, color_invert: bool = False, char_set_invert: bool = False, resize: bool = True, size: tuple = None):
+def convert_image(path2image: str,
+                  color_invert: bool = False, char_set_invert: bool = False,
+                  size: tuple = None) -> Image:
     global FileInfo, CharSet
     if not os.path.exists(path2image):
         return None
@@ -54,22 +41,20 @@ def convert_image(path2image: str, color_invert: bool = False, char_set_invert: 
     file: list = path2image.split("/")[-1].split(".")
     FileInfo = {"Name" : file[0], "Format" : file[1]}
     CharSet = CharSet[::-1 if not char_set_invert else 1]
-    if color_invert:
-        background: tuple = (255, 255, 255)
-        foreground: tuple = (0, 0, 0)
-    else:
-        background: tuple = (0, 0, 0)
-        foreground: tuple = (255, 255, 255)
 
-    original_image: Image = Image.open(path2image)
-    resized_image: Image = resize_image(original_image, size) if resize else original_image
-    gray_scale_image: Image = mono_image(resized_image)
+    image: Image = Image.open(path2image)
+    if size is None:
+        size = (round(image.width / CharSize["x"]), round(image.height / CharSize["y"]))
 
-    ascii_str: str = ascii_image(gray_scale_image)
+    resized_img, resized_txt = resize_image(image, size)
+    gray_scale_img: Image = mono_image(resized_img)
+    gray_scale_txt: Image = mono_image(resized_txt)
+    ascii_img: list[str] = ascii_image(gray_scale_img)
+    ascii_txt: list[str] = ascii_image(gray_scale_txt)
 
     os.makedirs(f"result/{FileInfo["Name"]}", exist_ok=True)
     txt = f"result/{FileInfo["Name"]}/{FileInfo["Name"]}.txt"
     with open(txt, "w", encoding="utf-8") as f:
-        f.write(ascii_str)
+        f.write("\n".join(ascii_txt))
     print(f"txt: {os.path.abspath(txt)}")
-    print(f"img: {ascii2image_monochrome(ascii_str, background, foreground)}")
+    print(f"img: {ascii2image_monochrome(ascii_img, color_invert)}")
