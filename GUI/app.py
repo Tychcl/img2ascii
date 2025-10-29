829909c1e67831d4f31cc7c6a305406e545c96ec
Tychcl <tocabloha@gmail.com>
2025-10-29 11:28:49 +0500

    вапвап

--- GUI/app.py
+++ GUI/app.py
@@ -1,6 +1,5 @@
 import customtkinter
-from customtkinter import filedialog
-from frames.scroll import ScrollFrame
+from widgets.OFD import OFD
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -10,16 +9,10 @@ class App(customtkinter.CTk):
         self.title("Ascii / Tychcl")
         self.geometry("400x400")
         self.iconbitmap("resources/icons/64.ico")
-        self.grid_columnconfigure((0, 1), weight=1)
+        self.grid_columnconfigure((0, 0), weight=1)
 
-        self.ImagePathButton = customtkinter.CTkButton(self, text="CTkButton", command=self.OpenFileDialog)
-        self.ImagePathButton.grid(row=0, column=0, padx=(3, 0), pady=3)
-
-    def OpenFileDialog(self):
-        self.ImagePath = filedialog.askopenfilename(
-        filetypes = (("Images",("*.png","*.bmp","*.jpg","*.jpeg")),
-                    ("PNG","*.png"),("BMP","*.bmp"),
-                    ("JPG","*.jpg"),("JPEG","*.jpeg")))
+        self.OFD = OFD(master=self)
+        self.OFD.grid(row=0, column=0, sticky="ew")
         
     def button_callback(self):
         print("button pressed")
